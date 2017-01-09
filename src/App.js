4ca90146e25d90c4145f47329092da307561c748
DaveG437 <davegardiner1@hotmail.com>
2017-01-09 16:43:55 +0000

    Get material-UI dialogue box working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 import './App.css';
 import Calendar from './Calendar.js';
+import Dialogue from './Dialogue';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Calendar></Calendar>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          {/*<Calendar></Calendar>*/}
+          <Dialogue/>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
